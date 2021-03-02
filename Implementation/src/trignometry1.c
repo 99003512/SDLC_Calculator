00bf32f6de382c876fe52bb09de8ab4a6e0a89d1
Karthik Manoj <karthikmanoj4@gmail.com>
2021-03-02 14:20:46 +0530

    a

--- Implementation/src/trignometry1.c
+++ Implementation/src/trignometry1.c
@@ -31,9 +31,9 @@ int trig(double angle)
   
    
   
-    printf("Sin(%lf) = %f\n", angle, sin(angle));  
-    printf("Cos(%lf) = %f\n", angle, cos(angle));  
-    printf("Tan(%lf) = %f\n", angle, tan(angle)); 
+    printf("Sin(%lf) = %f\n", angle, cal_sin(angle));  
+    printf("Cos(%lf) = %f\n", angle, cal_cos(angle));  
+    
     return 0;
 }
 int trignometry_test(float x)
@@ -44,7 +44,8 @@ int trignometry_test(float x)
 
   
  if((x==0)||(x==90)){
-        return 'error';
+     printf("error");
+        return 0;
  }
  
  return tan(angle); 
@@ -52,3 +53,53 @@ int trignometry_test(float x)
 }
 
 
+float cal_sin(float n) 
+{     
+    float accuracy = 0.0001, denominator, sinx, sinval; 
+      
+    // Converting degrees to radian 
+    n = n * (3.142 / 180.0);  
+  
+    float x1 = n; 
+      
+    // maps the sum along the series 
+    sinx = n;          
+      
+    // holds the actual value of sin(n) 
+    sinval = sin(n);     
+    int i = 1; 
+    do
+    { 
+        denominator = 2 * i * (2 * i + 1); 
+        x1 = -x1 * n * n / denominator; 
+        sinx = sinx + x1; 
+        i = i + 1; 
+    } while (accuracy <= fabs(sinval - sinx)); 
+    return sinx;
+} 
+
+
+int cal_cos(float n) 
+{ 
+    float accuracy = 0.0001, x1, denominator, cosx, cosval; 
+      
+    // Converting degrees to radian 
+    n = n * (3.142 / 180.0); 
+      
+    x1 = 1; 
+      
+    // maps the sum along the series 
+    cosx = x1;          
+      
+    // holds the actual value of sin(n) 
+    cosval = cos(n); 
+    int i = 1; 
+    do
+    { 
+        denominator = 2 * i * (2 * i - 1); 
+        x1 = -x1 * n * n / denominator; 
+        cosx = cosx + x1; 
+        i = i + 1; 
+    } while (accuracy <= fabs(cosval - cosx)); 
+    return cosx;
+} 
